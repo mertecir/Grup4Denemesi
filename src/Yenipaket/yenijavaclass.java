232e1ae00407d02489e74f44b8023523f76bf7a4
ygrp <yesimgarip9@gmail.com>
2022-07-06 21:46:22 +0300

    değişiklik yeşim

--- src/Yenipaket/yenijavaclass.java
+++ src/Yenipaket/yenijavaclass.java
@@ -3,5 +3,7 @@ package Yenipaket;
 public class yenijavaclass {
     public static void main(String[] args) {
         System.out.println("Merhabalar ben yeşim");
+        System.out.println("nasılsınız");
     }
+
 }
